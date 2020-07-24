190e3d134a239cc846bb525ef23f4b56348a8106
José González <deliriousdelta@gmail.com>
2020-07-23 20:47:36 -0500

    Objetos

---
+++ 1_Objetos/index.js
@@ -0,0 +1,15 @@
+let persona = {
+    nombre: 'Pepe',
+    edad: 22,
+    apellido: 'González',
+    pais: 'México',
+
+    saludar: function (){
+
+        console.log('Hola !')
+    }
+};
+
+persona.saludar;
+console.log(persona);
+
